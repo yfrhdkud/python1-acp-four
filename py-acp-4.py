8ef78a7a5ab84ed850c056344d3d909ea0d99086
EJA <eraajaswal@gmail.com>
2025-05-06 15:26:21 +0530

    Update py-acp-4.py

--- py-acp-4.py
+++ py-acp-4.py
@@ -1,13 +1,19 @@
 num = int(input("Enter a number: "))
+
 num_str = str(num)
 num_len = len(num_str)
 
 sum_of_powers = 0
-
 for digit in num_str:
-    sum_of_powers += int(digit) ** num_len
+    digit = int(digit)
+    power = 1
+    count = 0
+    while count < num_len:
+        power = power * digit
+        count = count + 1
+    sum_of_powers = sum_of_powers + power
 
 if sum_of_powers == num:
-    print(num, "is an Armstrong number!")
+    print("This is an Armstrong number")
 else:
-    print(num, "is not an Armstrong number.")
+    print("This is not an Armstrong number")
